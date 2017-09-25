989ccea7edc10512722e36484454aef96916cabe
gtm2122 <gabrielthomas281@gmail.com>
2017-09-25 10:40:59 -0400

    script to get the final annotation based on average position

---
+++ code/avg_anno.py
@@ -0,0 +1,9 @@
+# This script is to average the annotations given by deep matching
+
+import matplotlib.pyplot as plt
+import scipy.misc
+import numpy as np
+import os
+import shutil
+
+for i in os.listdir('/data/gabriel/deep_measure/matched_im/'):
